9848adb7cef420e1914f8080827bab32cf2dbd1c
sarahelshabrawy <hylrcs@gmail.com>
2021-08-25 21:43:29 +0200

    Asp.net Project

---
+++ PCI Analyzer/PCI Analyzer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PCI_Analyzer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
